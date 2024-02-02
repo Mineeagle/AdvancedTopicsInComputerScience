7cee326ee05775d2dbfbbb50c425dbe6b479dbce
mineeagle <141904026+Mineeagle@users.noreply.github.com.>
2024-02-02 11:19:59 +0100

    Bug fix

--- link_sender.py
+++ link_sender.py
@@ -5,4 +5,4 @@ class LinkSender:
     @staticmethod
     def send_link(data_dict, link):
         url = data_dict["maps_link_sending_url"]
-        return requests.post(url, json={"text": link})
+        return requests.post(url, json={"link": link})
