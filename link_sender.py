b48cc4640ad2d64297d4dbf6c153d57e0095e19b
mineeagle <141904026+Mineeagle@users.noreply.github.com.>
2024-02-02 11:15:11 +0100

    Update config.json

--- link_sender.py
+++ link_sender.py
@@ -4,5 +4,5 @@ import requests
 class LinkSender:
     @staticmethod
     def send_link(data_dict, link):
-        url = data_dict["maps_link_sending_link"]
+        url = data_dict["maps_link_sending_url"]
         return requests.post(url, json={"text": link})
