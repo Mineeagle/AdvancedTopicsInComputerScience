aa0436a0b4422b3c163a5f3dd6d9a3b734a33b15
mineeagle <141904026+Mineeagle@users.noreply.github.com.>
2024-02-02 11:00:08 +0100

    Send link to Davids server for further usage

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from cleaned_route_planning import RoutePlanning
+import requests
 
 def main():
     rp = RoutePlanning()
-    print(rp.get_google_maps_link())
+    link = rp.get_google_maps_link()
+    print(requests.post(url=r"https://altkleider.davidhojczyk.de/api/route/add", json={"text":link}))
 
 if __name__ == "__main__":
     main()
